6c76d11bb9cb22e425429c39448c44b55245c9e7
lhda-kingdom <lehuyducanh@gmail.com>
2022-06-09 10:49:48 +0700

    2nd com

--- abc.js
+++ abc.js
@@ -3,3 +3,7 @@ const axios = require('axios');
 const puppeteer = require('puppeteer');
 
 var browser = await puppeteer.launch({ headless: false });
+
+var browser2 = await puppeteer.launch({ headless: false });
+
+var browser3 = await puppeteer.launch({ headless: false });
